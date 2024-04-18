1a93a39fc47538e4dff929b1adbbaf9434085adc
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2024-04-18 16:19:24 +0200

    feat: calculating the nth Fibonacci number using Binet's formula.

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,22 @@ fn main() {
                 n,
                 fibonacci_iterative(n)
             );
+            println!(
+                "The {}st Fibonacci number using Binet's formula is: {}",
+                n,
+                fibonacci_binet(n)
+            );
             continue;
         } else if n == 2 {
             println!(
                 "The {}nd Fibonacci number using iteration is: {}",
                 n,
                 fibonacci_iterative(n)
+            );
+            println!(
+                "The {}nd Fibonacci number using Binet's formula is: {}",
+                n,
+                fibonacci_binet(n)
             );
             continue;
         } else if n == 3 {
@@ -36,12 +46,22 @@ fn main() {
                 n,
                 fibonacci_iterative(n)
             );
+            println!(
+                "The {}rd Fibonacci number using Binet's formula is: {}",
+                n,
+                fibonacci_binet(n)
+            );
             continue;
         } else {
             println!(
                 "The {}th Fibonacci number using iteration is: {}",
                 n,
                 fibonacci_iterative(n)
+            );
+            println!(
+                "The {}th Fibonacci number using Binet's formula is: {}",
+                n,
+                fibonacci_binet(n)
             );
             continue;
         }
@@ -67,4 +87,13 @@ fn fibonacci_iterative(n: u64) -> u64 {
     }
 
     y
+}
+
+fn fibonacci_binet(n: u64) -> u64 {
+    const SQUAREROOT_OF_FIVE: f64 = 2.236067977499790; // sqrt(5) ≈ 2.236067977499790
+    const PHI: f64 = 1.618033988749895; // φ = (1 + sqrt(5)) / 2 ≈ 1.618033988749895 (the golden ratio)
+    const PSI: f64 = -0.618033988749895; // ψ = (1 - sqrt(5)) / 2 ≈ -0.618033988749895
+
+    // Fn = (φ^n - ψ^n) / sqrt(5)
+    return ((PHI.powf(n as f64) - PSI.powf(n as f64)) / SQUAREROOT_OF_FIVE) as u64;
 }
